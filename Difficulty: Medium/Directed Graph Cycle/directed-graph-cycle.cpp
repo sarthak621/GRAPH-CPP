0497949fefcd7e14645e56cbcc7a4bb286bea864
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-05-15 23:57:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+  
+    bool dfs(int node,int V, vector<vector<int>> &adj,vector<int> &vis,vector<int> &pathVis){
+        vis[node]=1;
+        pathVis[node]=1;
+        
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                if(dfs(it,V,adj,vis,pathVis)==true){
+                    return true;
+                }
+            }
+            else if(pathVis[it]){
+                    return true;
+                }
+        }
+        
+        pathVis[node]=0;
+        return false;
+    }
+    
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        // Step 1: Convert edge list to adjacency list
+        vector<vector<int>> adj(V);
+        for(auto &e : edges){
+            adj[e[0]].push_back(e[1]);
+        }
+
+        // Step 2: Use DFS to detect cycle
+        //visited array
+        vector<int>vis(V,0);
+        // path visited
+        vector<int>pathVis(V,0);
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(dfs(i,V,adj,vis,pathVis)==true){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        cin.ignore();
+        vector<vector<int>> edges;
+        for (int i = 1; i <= E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u, v});
+        }
+
+        Solution obj;
+        bool ans = obj.isCyclic(V, edges);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
