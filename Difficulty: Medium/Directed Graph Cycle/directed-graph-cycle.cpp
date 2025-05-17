a82e9e5dd51a1c886f95e4787136c1f2c03facc6
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-05-17 23:11:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -7,50 +7,106 @@ using namespace std;
 
 class Solution {
   public:
-  
-    bool dfs(int node,int V, vector<vector<int>> &adj,vector<int> &vis,vector<int> &pathVis){
-        vis[node]=1;
-        pathVis[node]=1;
-        
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                if(dfs(it,V,adj,vis,pathVis)==true){
-                    return true;
-                }
-            }
-            else if(pathVis[it]){
-                    return true;
-                }
-        }
-        
-        pathVis[node]=0;
-        return false;
-    }
-    
     bool isCyclic(int V, vector<vector<int>> &edges) {
-        // Step 1: Convert edge list to adjacency list
-        vector<vector<int>> adj(V);
-        for(auto &e : edges){
-            adj[e[0]].push_back(e[1]);
-        }
-
-        // Step 2: Use DFS to detect cycle
-        //visited array
-        vector<int>vis(V,0);
-        // path visited
-        vector<int>pathVis(V,0);
-        
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,V,adj,vis,pathVis)==true){
-                    return true;
+        // code here
+        //finding directed graph using bfs using kahn's algo 
+        //converting the list into adjacency list
+            vector<int>adj[V+1];
+            for(int i=0;i<edges.size();i++){
+                int u = edges[i][0];
+                int v = edges[i][1];
+                adj[u].push_back(v);
+            }
+            
+            //making the indegree array and filling it
+            vector<int>indegree(V,0);
+            
+            for(int i=0;i<V;i++){
+                for(auto it:adj[i]){
+                    indegree[it]++;
                 }
             }
-        }
-        return false;
-        
+            vector<int>topo; //for storing the ans
+            queue<int>q;
+            
+            for(int i=0;i<V;i++){
+                if(indegree[i]==0){
+                    q.push(i);
+                    
+                }
+            }
+            
+            while(!q.empty()){
+                int node=q.front();
+                q.pop();
+                topo.push_back(node);
+                
+                for(auto it: adj[node]){
+                    indegree[it]--;
+                    
+                    if(indegree[it]==0){
+                        q.push(it);
+                    }
+                }
+            }
+       if(topo.size()==V) return false;
+    
+        return true;  
     }
 };
+
+
+
+
+
+// finding directed graph using dfs
+
+    // class Solution {
+//   public:
+  
+//     bool dfs(int node,int V, vector<vector<int>> &adj,vector<int> &vis,vector<int> &pathVis){
+//         vis[node]=1;
+//         pathVis[node]=1;
+        
+//         for(auto it: adj[node]){
+//             if(!vis[it]){
+//                 if(dfs(it,V,adj,vis,pathVis)==true){
+//                     return true;
+//                 }
+//             }
+//             else if(pathVis[it]){
+//                     return true;
+//                 }
+//         }
+        
+//         pathVis[node]=0;
+//         return false;
+//     }
+    
+//     bool isCyclic(int V, vector<vector<int>> &edges) {
+//         // Step 1: Convert edge list to adjacency list
+//         vector<vector<int>> adj(V);
+//         for(auto &e : edges){
+//             adj[e[0]].push_back(e[1]);
+//         }
+
+//         // Step 2: Use DFS to detect cycle
+//         //visited array
+//         vector<int>vis(V,0);
+//         // path visited
+//         vector<int>pathVis(V,0);
+        
+//         for(int i=0;i<V;i++){
+//             if(!vis[i]){
+//                 if(dfs(i,V,adj,vis,pathVis)==true){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+        
+//     }
+// };
 
 
 //{ Driver Code Starts.
