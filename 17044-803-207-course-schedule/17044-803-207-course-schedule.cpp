777224381072cb1ddfdb978a6fa8836c4da5dcbd
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-05-17 23:32:49 +0530

    Time: 6 ms, Memory: 18.8 MB - LeetHub

---
+++ 17044-803-207-course-schedule/17044-803-207-course-schedule.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+            //finding directed graph using bfs using kahn's algo 
+        //converting the list into adjacency list
+            vector<int>adj[numCourses+1];
+            for(int i=0;i<prerequisites.size();i++){
+                int u = prerequisites[i][0];
+                int v = prerequisites[i][1];
+                adj[u].push_back(v);
+            }
+            
+            //making the indegree array and filling it
+            vector<int>indegree(numCourses,0);
+            
+            for(int i=0;i<numCourses;i++){
+                for(auto it:adj[i]){
+                    indegree[it]++;
+                }
+            }
+            vector<int>topo; //for storing the ans
+            queue<int>q;
+            
+            for(int i=0;i<numCourses;i++){
+                if(indegree[i]==0){
+                    q.push(i);
+                    
+                }
+            }
+            
+            while(!q.empty()){
+                int node=q.front();
+                q.pop();
+                topo.push_back(node);
+                
+                for(auto it: adj[node]){
+                    indegree[it]--;
+                    
+                    if(indegree[it]==0){
+                        q.push(it);
+                    }
+                }
+            }
+       if(topo.size()==numCourses) return true;
+    
+        return false;  
+    }
+};
